47d50fa47424dc929cfeec39c2e8b0b10872ca8e
Ankitdhuria09 <ankitdhuria00@gmail.com>
2024-11-07 13:49:53 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,73 +6,75 @@
 #include <fstream>
 #include <unordered_set>
 #include <iomanip>
+#include <set>
+
 using namespace std;
 
 static vector<string> bookNames = {
-    "Cats by Moncrif.txt",
-    "Foxes Book of Martyrs Part 1.txt",
-    "Foxes Book of Martyrs Part 2.txt",
-    "Foxes Book of Martyrs Part 3.txt",
-    "Foxes Book of Martyrs Part 4.txt",
-    "Foxes Book of Martyrs Part 5.txt",
-    "Foxes Book of Martyrs Part 6.txt",
-    "Gerards Herbal Vol. 1.txt",
-    "Gerards Herbal Vol. 2.txt",
-    "Gerard's Herbal Vol. 3.txt",
-    "Gerards Herbal Vol.4.txt",
-    "Gil Blas.txt",
-    "Gossip in a Library.txt",
-    "Hudibras.txt",
-    "King of the Beggars.txt",
-    "Knocknagow.txt",
-    "Les Chats par Moncrif.txt",
-    "Love and Madness - Herbert Croft.txt",
-    "Lives and Anecdotes of Misers.txt",
-    "Memoirs of Laetitia Pilkington V 1.txt",
-    "Memoirs of Laetitia Pilkington V 2.txt",
-    "Memoirs of Laetitia Pilkington V 3.txt",
-    "Memoirs of Mrs Margaret Leeson - Peg Plunkett.txt",
-    "Monro his Expedition.txt",
-    "Mrs Beetons Book of Household Management.txt",
-    "Out of the Hurly-Burly.txt",
-    "Percys Reliques.txt",
-    "Pompey the Little.txt",
-    "Radical Pamphlets from the English Civil War.txt",
-    "Scepsis Scientifica.txt",
-    "The Anatomy of Melancholy Part 1.txt",
-    "The Anatomy of Melancholy Part 2.txt",
-    "The Anatomy of Melancholy Part 3.txt",
-    "The Complete Cony-catching.txt",
-    "The Consolation of Philosophy.txt",
-    "The Covent Garden Calendar.txt",
-    "The Devil on Two Sticks.txt",
-    "The Diary of a Lover of Literature.txt",
-    "The History Of Ireland - Geoffrey Keating.txt",
-    "The History of the Human Heart.txt",
-    "The Ingoldsby Legends.txt",
-    "The Life of Beau Nash.txt",
-    "The Life of john Buncle by Thomas Amory.txt",
-    "The Life of King Richard III.txt",
-    "The Life of Pico della Mirandola.txt",
-    "The Martyrdom of Man.txt",
-    "The Masterpiece of Aristotle.txt",
-    "The Memoirs of Count Boruwlaski.txt",
-    "The Metamorphosis of Ajax.txt",
-    "The Newgate Calendar - Supplement 3.txt",
-    "The Newgate Calendar Supplement 2.txt",
-    "The Newgate Calendar Supplement.txt",
-    "The Newgate Calendar V 1.txt",
-    "The Newgate Calendar V 2.txt",
-    "The Newgate Calendar V 3.txt",
-    "The Newgate Calendar V 4.txt",
-    "The Newgate Calendar V 5.txt",
-    "The Newgate Calendar V 6.txt",
-    "The Poems of Ossian.txt",
-    "The Poetical Works of John Skelton.txt",
-    "The Protestant Reformation.txt",
-    "The Real Story of John Carteret Pilkington.txt",
-    "The Rowley Poems.txt",
-    "The Silver Fox.txt"
+"Cats by Moncrif.txt",
+  "Foxes Book of Martyrs Part 1.txt",
+  "Foxes Book of Martyrs Part 2.txt",
+  "Foxes Book of Martyrs Part 3.txt",
+  "Foxes Book of Martyrs Part 4.txt",
+  "Foxes Book of Martyrs Part 5.txt",
+  "Foxes Book of Martyrs Part 6.txt",
+  "Gerards Herbal Vol. 1.txt",
+  "Gerards Herbal Vol. 2.txt",
+  "Gerard's Herbal Vol. 3.txt",
+  "Gerards Herbal Vol.4.txt",
+  "Gil Blas.txt",
+  "Gossip in a Library.txt",
+  "Hudibras.txt",
+  "King of the Beggars.txt",
+  "Knocknagow.txt",
+  "Les Chats par Moncrif.txt",
+  "Love and Madness - Herbert Croft.txt",
+  "Lives and Anecdotes of Misers.txt",
+  "Memoirs of Laetitia Pilkington V 1.txt",
+  "Memoirs of Laetitia Pilkington V 2.txt",
+  "Memoirs of Laetitia Pilkington V 3.txt",
+  "Memoirs of Mrs Margaret Leeson - Peg Plunkett.txt",
+  "Monro his Expedition.txt",
+  "Mrs Beetons Book of Household Management.txt",
+  "Out of the Hurly-Burly.txt",
+  "Percys Reliques.txt",
+  "Pompey the Little.txt",
+  "Radical Pamphlets from the English Civil War.txt",
+  "Scepsis Scientifica.txt",
+  "The Anatomy of Melancholy Part 1.txt",
+  "The Anatomy of Melancholy Part 2.txt",
+  "The Anatomy of Melancholy Part 3.txt",
+  "The Complete Cony-catching.txt",
+  "The Consolation of Philosophy.txt",
+  "The Covent Garden Calendar.txt",
+  "The Devil on Two Sticks.txt",
+  "The Diary of a Lover of Literature.txt",
+  "The History Of Ireland - Geoffrey Keating.txt",
+  "The History of the Human Heart.txt",
+  "The Ingoldsby Legends.txt",
+  "The Life of Beau Nash.txt",
+  "The Life of john Buncle by Thomas Amory.txt",
+  "The Life of King Richard III.txt",
+  "The Life of Pico della Mirandola.txt",
+  "The Martyrdom of Man.txt",
+  "The Masterpiece of Aristotle.txt",
+  "The Memoirs of Count Boruwlaski.txt",
+  "The Metamorphosis of Ajax.txt",
+  "The Newgate Calendar - Supplement 3.txt",
+  "The Newgate Calendar Supplement 2.txt",
+  "The Newgate Calendar Supplement.txt",
+  "The Newgate Calendar V 1.txt",
+  "The Newgate Calendar V 2.txt",
+  "The Newgate Calendar V 3.txt",
+  "The Newgate Calendar V 4.txt",
+  "The Newgate Calendar V 5.txt",
+  "The Newgate Calendar V 6.txt",
+  "The Poems of Ossian.txt",
+  "The Poetical Works of John Skelton.txt",
+  "The Protestant Reformation.txt",
+  "The Real Story of John Carteret Pilkington.txt",
+  "The Rowley Poems.txt",
+  "The Silver Fox.txt"
 };
 
 class BookAnalyzer {
@@ -83,7 +85,7 @@ private:
     set<string> excludedWords;
 
     void initializeExcludedWords() {
-        excludedWords = {"A","AN","THE","OF","IN"};
+        excludedWords = {"A", "AN", "THE", "OF", "IN"};
     }
 
 public:
@@ -93,9 +95,8 @@ public:
 
     void loadBookWords() {
         cout << "Loading books..." << endl;
-
         for (const string& bookName : bookNames) {
-            ifstream file("books/" + bookName);
+            ifstream file(bookName);
             if (file) {
                 vector<string> words;
                 string word;
@@ -118,7 +119,6 @@ public:
 
     void countWords() {
         cout << "Counting words..." << endl;
-
         for (const auto& [bookName, words] : bookWords) {
             map<string, int> counts;
             for (const string& word : words) {
@@ -132,7 +132,6 @@ public:
 
     void calculateNormalizedScores() {
         cout << "Calculating normalized word scores..." << endl;
-
         for (const auto& [bookName, counts] : wordCountMap) {
             map<string, double> scores;
             int totalWords = counts.at("total");
@@ -149,14 +148,11 @@ public:
 
     void findTopWords() {
         cout << "Identifying the top words..." << endl;
-
         ofstream outFile("outputFiles/common_words.txt");
-        outFile << "Most Common Words in Each Book\n";
-        outFile << "=================================\n\n";
+        outFile << "Most Common Words in Each Book\n=================================\n\n";
 
         for (const auto& [bookName, scores] : normalizedScores) {
             vector<pair<string, double>> sortedWords(scores.begin(), scores.end());
-
             sort(sortedWords.begin(), sortedWords.end(),
                  [](const pair<string, double>& a, const pair<string, double>& b) {
                      return a.second > b.second;
@@ -177,7 +173,6 @@ public:
 
     void compareBooks() {
         cout << "Comparing books..." << endl;
-
         ofstream outFile("outputFiles/similarity_matrix.txt");
         if (!outFile) {
             cerr << "Error opening outputFiles/similarity_matrix.txt for writing" << endl;
@@ -185,10 +180,10 @@ public:
         }
 
         outFile << "Book Similarity Matrix\n" << string(100, '=') << endl << endl;
-
         vector<string> bookList;
-        for (const auto& pair : normalizedScores) {
-            bookList.push_back(pair.first);
+
+        for (const auto& [bookName, _] : normalizedScores) {
+            bookList.push_back(bookName);
         }
 
         for (size_t i = 0; i < bookList.size(); ++i) {
@@ -196,11 +191,14 @@ public:
                 const string& book1 = bookList[i];
                 const string& book2 = bookList[j];
 
-                unordered_set<string> wordsInBook1(normalizedScores[book1].begin(), normalizedScores[book1].end());
-                int matchCount = 0;
+                unordered_set<string> wordsInBook1;
+                for (const auto& [word, score] : normalizedScores[book1]) {
+                    wordsInBook1.insert(word);
+                }
 
-                for (const auto& word : normalizedScores[book2]) {
-                    if (wordsInBook1.count(word.first)) {
+                int matchCount = 0;
+                for (const auto& [word, score] : normalizedScores[book2]) {
+                    if (wordsInBook1.count(word)) {
                         matchCount++;
                     }
                 }
@@ -217,7 +215,6 @@ public:
 
     void findSimilarBooks() {
         cout << "Finding the top 10 similar books..." << endl;
-
         vector<pair<string, double>> similarities;
         ifstream inFile("outputFiles/similarity_matrix.txt");
         string line;
@@ -238,8 +235,7 @@ public:
              });
 
         ofstream outFile("outputFiles/top_similar_books.txt");
-        outFile << "Top 10 Similar Books\n";
-        outFile << "=====================\n\n";
+        outFile << "Top 10 Similar Books\n=====================\n\n";
 
         for (size_t i = 0; i < min(static_cast<size_t>(10), similarities.size()); ++i) {
             outFile << (i + 1) << ". " << similarities[i].first << " | Similarity: " << fixed << setprecision(4) << similarities[i].second << endl;
